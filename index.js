d4ffd479aa463a2dcbd781e57b311ad2425bb593
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-18 15:48:44 +0100

    Files renamed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { getUsers, getUsers2, getUsers3 } from './1. Peticiones/1-index_GET.js'
-import { createUser2, createUser3, createUser3_objectOutside } from './1. Peticiones/2-index_POST.js'
-import { updateUser_PUT, updateUser_PATCH } from './1. Peticiones/3-index_PUT_PATCH.js'
-import { deleteUser } from './1. Peticiones/4-index_DELETE.js'
+import { getUsers, getUsers2, getUsers3 } from './1. Simple requests/1-index_GET.js'
+import { createUser2, createUser3, createUser3_objectOutside } from './1. Simple requests/2-index_POST.js'
+import { updateUser_PUT, updateUser_PATCH } from './1. Simple requests/3-index_PUT_PATCH.js'
+import { deleteUser } from './1. Simple requests/4-index_DELETE.js'
 
 
 createUser2()
